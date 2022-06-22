69cccd6e3e2002eb31cd5cb7103df216994eb2bd
brunotiago7 <bruno_tiago7@outlook.com>
2022-06-22 08:17:26 -0300

    Primeiro Projeto

---
+++ incial.js
@@ -0,0 +1,16 @@
+/* var pão;
+var broa;
+var pãesvendidos;
+var broasvendidas;
+var total;
+var poupança;
+pão = 500;
+broa = 250;
+pãesvendidos = pão * 0, 12;
+broasvendidas = broa * 1, 50;
+total = pãesvendidos + broasvendidas;
+poupança = total / 10
+console.log("O valor a ser guardado na poupança é de " + poupança + " reais.")
+ */
+
+
